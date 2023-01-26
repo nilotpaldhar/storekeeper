f2828ccf7d39d1b1fe291304bdc58ef6cab0faad
Nilotpal Dhar <nilotpaldhar1997@gmail.com>
2023-01-26 20:57:10 +0530

    feat(rating): add rating component
    fix #18

--- src/components/data-entry/index.js
+++ src/components/data-entry/index.js
@@ -1,4 +1,5 @@
 export { default as Checkbox } from '@ui/data-entry/Checkbox';
 export { default as Radio } from '@ui/data-entry/Radio';
+export { default as Rating } from '@ui/data-entry/Rating';
 export { default as Switch } from '@ui/data-entry/Switch';
 export { default as TextField } from '@ui/data-entry/TextField';
