66e593c76d941b3874d988511aff1571d21c1e43
Nilotpal Dhar <nilotpaldhar1997@gmail.com>
2023-01-28 12:31:42 +0530

    feat(select): add select input component
    fix #15

---
+++ src/components/data-entry/Select/SelectItem/styles.cva.js
@@ -0,0 +1,12 @@
+import { cva } from 'class-variance-authority';
+
+export const rootStyles = cva([
+	'px-3 py-2 flex items-center text-neutral-900 transition duration-300 cursor-pointer',
+	'data-[disabled]:opacity-50 data-[disabled]:cursor-not-allowed',
+	'data-[highlighted]:bg-neutral-50 data-[highlighted]:outline-none',
+	'data-[state=checked]:bg-neutral-200 data-[state=checked]:font-semibold',
+]);
+
+export const indicatorStyles = cva(['flex items-center pr-2']);
+
+export default rootStyles;
---
+++ src/components/data-entry/Select/styles.cva.js
@@ -0,0 +1,21 @@
+import { cva } from 'class-variance-authority';
+
+export const triggerStyles = cva([
+	'form-input w-full flex items-center justify-between p-3 border border-neutral-200 transition duration-300 bg-white',
+	'hover:border-neutral-900 hover:text-current focus:ring-primary-600 focus:border-primary-600',
+	'text-sm text-neutral-700 font-normal leading-tight whitespace-nowrap overflow-hidden select-none',
+	'data-[disabled]:bg-neutral-200 data-[disabled]:hover:border-neutral-200',
+]);
+
+export const contentStyles = cva([
+	'bg-white shadow-lg border border-neutral-200',
+	'w-[var(--radix-select-trigger-width)]',
+	'max-h-[var(--radix-select-content-available-height)]',
+]);
+
+export const labelStyles = cva('flex items-center max-w-max text-neutral-900 mb-2');
+
+export const labelTextStyles = cva([
+	'text-sm font-medium select-none leading-none',
+	'[&>sup]:text-xs [&>sup]:-top-[0.2em] [&>sup]:left-[0.2em]',
+]);
