12144178ba3ed5cf54f4659dd624e0c6e765c220
Nilotpal Dhar <nilotpaldhar1997@gmail.com>
2023-11-02 09:56:30 +0530

    fix(error-page): modify the error page layout

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,68 +1,56 @@
-import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 
-/** Components & Icons. */
-import Anchor from '@ui/general/Anchor';
+import Seo from '@ui/general/Seo';
+import Image from '@ui/data-display/Image';
 import Container from '@ui/general/Container';
 import RegularButton from '@ui/buttons/RegularButton';
-import LayoutWrapper from '@ui/layouts/LayoutWrapper';
-import ArrowLeftIcon from '@icons/regular/ArrowLeft';
 
-/** Functions. */
-import fetchPage from '@libs/general/dynamic-page/fetchPage';
+import ArrowLeftIcon from '@icons/regular/ArrowLeft';
+import error404Img from '@public/404.svg';
 
 /**
  * Render the Error404 component.
  *
  * @return {Element} The Error404 component.
  */
-const Error404 = ({ page: { title, description } }) => (
-	<div className="py-28">
-		<Container>
-			<div className="flex flex-col items-center max-w-xl mx-auto">
-				<div className="text-center">
-					<h1 className="text-3xl font-black leading-normal text-primary-600 lg:text-4xl">
-						{title}
-					</h1>
-					<div className="flex flex-col space-y-1 text-base font-light text-neutral-900 mt-2">
-						{description && <p>{description}</p>}
-						<p className="flex flex-wrap justify-center text-center items-center space-x-1">
-							<span>Visit the</span>
-							<Anchor href="/">Homepage</Anchor>
-							<span>or</span>
-							<Anchor href="/contact">Contact Us</Anchor>
-							<span>about the problem</span>
-						</p>
+const Error404 = () => {
+	const router = useRouter();
+
+	return (
+		<>
+			<Seo title="404 Not Found" />
+			<div className="flex items-center justify-center h-full max-h-screen overflow-hidden">
+				<Container>
+					<div className="flex items-center flex-col space-y-6 md:space-y-8 max-w-2xl mx-auto">
+						<div>
+							<Image src={error404Img} alt="404 Not Found" />
+						</div>
+						<div className="text-neutral-900">
+							<h1 className="text-3xl md:text-4xl xl:text-5xl font-normal text-center leading-tight capitalize">
+								Sorry Something is Missing
+							</h1>
+							<p className="text-lg md:text-xl font-light text-center max-w-md mx-auto leading-relaxed mt-4">
+								The link you clicked may be broken or the page may have been removed.
+							</p>
+							<div className="flex items-center flex-wrap justify-center gap-4 mt-8">
+								<RegularButton as="anchor" href="/" className="!px-6 md:!px-14">
+									Go Home
+								</RegularButton>
+								<RegularButton
+									intent="light"
+									startIcon={ArrowLeftIcon}
+									className="!px-6 md:!px-14"
+									onClick={() => router.back()}
+								>
+									Go Back
+								</RegularButton>
+							</div>
+						</div>
 					</div>
-				</div>
-				<RegularButton as="anchor" href="/" startIcon={ArrowLeftIcon} className="!px-8 mt-5">
-					Back To Home
-				</RegularButton>
+				</Container>
 			</div>
-		</Container>
-	</div>
-);
-
-/**
- * Prop Types.
- */
-Error404.propTypes = {
-	page: PropTypes.shape({
-		title: PropTypes.string,
-		description: PropTypes.string,
-	}).isRequired,
-};
-
-/** Page Layout. */
-Error404.getLayout = (page, data) => <LayoutWrapper data={data}>{page}</LayoutWrapper>;
-
-/**
- * Get page props.
- *
- * @return {object} Page props.
- */
-export const getStaticProps = async ({ preview }) => {
-	const data = await fetchPage(preview, 'errorpage');
-	return { props: { data }, revalidate: 10 };
+		</>
+	);
 };
 
 export default Error404;
--- src/pages/500.jsx
+++ src/pages/500.jsx
@@ -1,28 +1,47 @@
+import { useRouter } from 'next/router';
+
 import Seo from '@ui/general/Seo';
 import Container from '@ui/general/Container';
+import RegularButton from '@ui/buttons/RegularButton';
+
+import Reloadcon from '@icons/regular/Reload';
 
 /**
  * Render the Error500 component.
  *
  * @return {Element} The Error500 component.
  */
-const Error500 = () => (
-	<>
-		<Seo title="Internal Server Error" />
-		<div className="h-full py-10">
-			<Container className="flex items-center justify-center h-full">
-				<div className="max-w-xl mx-auto text-center">
-					<h1 className="text-3xl font-black leading-normal text-primary-600 lg:text-4xl">
-						Opps! Something went wrong
-					</h1>
-					<p className="text-base font-light leading-relaxed text-neutral-900 mt-2">
-						The page you are trying to access is temporarily unavailable due to an internal server
-						error. Please try again later or reload the page.
-					</p>
-				</div>
-			</Container>
-		</div>
-	</>
-);
+const Error500 = () => {
+	const router = useRouter();
+
+	return (
+		<>
+			<Seo title="Internal Server Error" />
+			<div className="flex items-center justify-center h-full max-h-screen overflow-hidden">
+				<Container>
+					<div className="max-w-3xl mx-auto text-neutral-900">
+						<h1 className="text-3xl md:text-4xl xl:text-5xl font-normal text-center leading-tight capitalize">
+							Opps! Something went wrong
+						</h1>
+						<p className="text-lg md:text-xl font-light text-center max-w-2xl mx-auto leading-relaxed mt-4">
+							The page you are trying to access is temporarily unavailable due to an internal server
+							error. Please <strong>try again later</strong> or <strong>reload the page</strong>
+						</p>
+						<div className="flex justify-center mt-8">
+							<RegularButton
+								intent="light"
+								startIcon={Reloadcon}
+								className="!px-6 md:!px-14"
+								onClick={() => router.reload()}
+							>
+								Reload
+							</RegularButton>
+						</div>
+					</div>
+				</Container>
+			</div>
+		</>
+	);
+};
 
 export default Error500;
