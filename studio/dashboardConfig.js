d6028dd19029ce4ad496d9eb9845ff0401ecfdff
Nilotpal Dhar <nilotpaldhar1997@gmail.com>
2024-01-07 10:29:18 +0530

    fix(sanity-studio): remove the 'document-list-widget', which has caused the Sanity Studio to crash

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -1,16 +1,5 @@
 import { projectUsersWidget, projectInfoWidget } from '@sanity/dashboard';
-import { documentListWidget } from 'sanity-plugin-dashboard-widget-document-list';
 
 export default {
-	widgets: [
-		documentListWidget({
-			title: 'Recently edited',
-			order: '_updatedAt desc',
-			types: ['homepage', 'page', 'errorpage'],
-			limit: 10,
-			layout: { width: 'auto', height: 'small' },
-		}),
-		projectUsersWidget(),
-		projectInfoWidget(),
-	],
+	widgets: [projectUsersWidget(), projectInfoWidget()],
 };
