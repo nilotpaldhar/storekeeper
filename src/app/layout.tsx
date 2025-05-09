9cef2e05ea76a688759c403a328e991374a7f966
Nilotpal Dhar <nilotpaldhar1997@gmail.com>
2025-05-09 21:46:39 +0530

    feat(ui): initialize shadcn/ui and update tailwind theme with custom CSS variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,17 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { Nunito as FontSans } from "next/font/google";
+import { cn } from "@/utils/general/cn";
+
+import "@/styles/global.css";
+
+// Initializing the Nunito font with specific subsets and variable name
+const fontSans = FontSans({
+	subsets: ["latin"],
+	variable: "--font-sans",
+	fallback: ["sans-serif"],
+	style: ["normal", "italic"],
+	weight: ["300", "400", "500", "600", "700", "800"],
+});
 
 export const metadata: Metadata = {
 	title: "StoreKeeper",
@@ -13,7 +25,7 @@ const RootLayout = ({
 }>) => {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body className={cn("antialiased", fontSans.variable)}>{children}</body>
 		</html>
 	);
 };
